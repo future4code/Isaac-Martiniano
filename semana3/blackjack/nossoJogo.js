7fa982b350b6bdfba37627765cad120c3d88ba15
isaac <isaacam517@gmail.com>
2020-04-03 16:38:57 -0300

    Desafio que nada fiz, infelizmente

---
+++ semana3/blackjack/nossoJogo.js
@@ -0,0 +1,36 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+console.log("Bem vindo ao jogo Blackjack!")
+if (confirm("Quer Iniciar o Jogo?")){
+   const carta = comprarCarta(); 
+// Sorteia uma carta. Por exemplo, o rei de ouros
+   const usuario = 0
+   const maquina = 0
+
+if {}
+
+console.log(carta.texto) 
+// imprime o texto da carta. Nesse caso: "K♦️"
+
+console.log(carta.valor) 
+// imprime o valor da carta (um número). Nesse caso: 10)
+
+
+
+}
+else {
+   console.log("O jogo acabou")
+
+}
+
+
