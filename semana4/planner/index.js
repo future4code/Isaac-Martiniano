18c4715245de37e4301db920b8ec1ef6b29db132
isaac <isaacam517@gmail.com>
2020-04-07 20:02:18 -0300

    atividade adicionando as tarefas

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,10 +1,30 @@
 
 function adicionarTarefa() {
-    const meuInput = document.getElementById("meu-input")
+    const meuInput = document.getElementById("novaTarefa")
+    const diaSemana = document.getElementById("dia-da-semana")
+    
+    const Domingo = document.getElementById("domingo")
     const Segunda = document.getElementById("segunda")
-    if (meuInput.value === "" || meuInput.value === " "){
-        alert("Digite um Item válido")
-    } else {
-        Segunda.innerHTML += "<li>" + meuInput.value + "</li>";
-    }
+    const Terca = document.getElementById("terca")
+    const Quarta = document.getElementById("quarta")
+    const Quinta = document.getElementById("quinta")
+    const Sexta = document.getElementById("sexta")
+    const Sabado = document.getElementById("sabado")
+    
+
+    if (diaSemana.value === "domingo"){
+        Domingo.innerHTML += "<li>" + meuInput.value + "</li>"
+    }else if (diaSemana.value === "segunda"){
+        Segunda.innerHTML += "<li>" + meuInput.value + "</li>"
+    }else if (diaSemana.value === "terca") {
+        Terca.innerHTML += "<li>" + meuInput.value + "</li>"
+    }else if (diaSemana.value === "quarta") {
+        Quarta.innerHTML += "<li>" + meuInput.value + "</li>"
+    }else if (diaSemana.value === "quinta") {
+        Quinta.innerHTML += "<li>" + meuInput.value + "</li>"    
+    }else if (diaSemana.value === "sexta") {
+        Sexta.innerHTML += "<li>" + meuInput.value + "</li>"    
+    }else if (diaSemana.value === "sabado") {
+        Sabado.innerHTML += "<li>" + meuInput.value + "</li>"
+    }        
 }
