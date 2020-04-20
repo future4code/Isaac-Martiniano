53b3b24a6791a4d1c47cf93e7deab7ba5c359a7f
isaac <isaacam517@gmail.com>
2020-04-20 17:26:04 -0300

    criacao-de-formulario

---
+++ semana5/labedin/labedin/build/precache-manifest.cf3df45fcf2c3043d6f2120b85cf43eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecdc0ec2bff2abd60a322e347613051c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e6c413fa41b21b130b9",
+    "url": "/static/css/main.a2ed01d4.chunk.css"
+  },
+  {
+    "revision": "dd35cab1ec671b13bbed",
+    "url": "/static/js/2.65cf7749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65cf7749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6c413fa41b21b130b9",
+    "url": "/static/js/main.8ffce81f.chunk.js"
+  },
+  {
+    "revision": "9cec4286d4e96efb4903",
+    "url": "/static/js/runtime-main.6b07746c.js"
+  }
+]);
