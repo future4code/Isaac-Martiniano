95ab88188b64a90d68fb93e82b6fe3142f1f1a23
isaac <isaacam517@gmail.com>
2020-05-18 16:49:28 -0300

    diagrama-wireframe

---
+++ semana7/integracao-de-apis/integracao-de-apis/build/precache-manifest.50967cea8671d71833e97b93dce1f26a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ca0f3de7c14905860eb1a398ca632f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5a4287e2a51d7964ae",
+    "url": "/static/css/main.1359fca2.chunk.css"
+  },
+  {
+    "revision": "090db5f3d35e5a17d26b",
+    "url": "/static/js/2.c899a1cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c899a1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5a4287e2a51d7964ae",
+    "url": "/static/js/main.02eb3a32.chunk.js"
+  },
+  {
+    "revision": "1c5efca044a5b0f43f48",
+    "url": "/static/js/runtime-main.91c2e442.js"
+  }
+]);
